902f05e0357dc35fdd5de1579f160a65fb71de8c
AlienX <115051132+Sharrabbha1@users.noreply.github.com>
2024-06-07 20:06:10 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React Website</h1>
+        <p>This is a basic React website created as a starting point.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Welcome to My React Website</h1>
+      <p>This is a basic React website created as a starting point.</p>
+    </header>
+  );
+}
+
+export default Header;
